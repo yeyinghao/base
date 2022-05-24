74c118a094d2b3a89972177219d70e93ea7be0ba
yeyinghao <1113531030@qq.com>
2022-05-24 22:12:09 +0800

    创建模块

---
+++ app/bootstrap/src/main/java/com/yyh/base/biz/BootstrapApplication.java
@@ -0,0 +1,20 @@
+package com.yyh.base.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+/**
+ * 程序入口-启动类
+ * @author yeyinghao
+ * @version 1.0.0
+ * @date: 2022/5/24 21:54
+ * @description: BootstrapApplication.java
+ */
+@SpringBootConfiguration
+public class BootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapApplication.class, args);
+	}
+
+}
