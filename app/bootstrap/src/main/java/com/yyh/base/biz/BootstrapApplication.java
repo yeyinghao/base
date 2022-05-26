31a792df1d72d0226c6ad2b5acca2a008bc60e99
yeyinghao <1113531030@qq.com>
2022-05-26 13:51:04 +0800

    创建模块

--- app/bootstrap/src/main/java/com/yyh/base/biz/BootstrapApplication.java
+++ app/bootstrap/src/main/java/com/yyh/base/biz/BootstrapApplication.java
@@ -1,6 +1,8 @@
 package com.yyh.base.biz;
 
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -18,9 +20,12 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class BootstrapApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(BootstrapApplication.class);
+
 	public static void main(String[] args) {
 		try {
 			SpringApplication.run(BootstrapApplication.class, args);
+			logger.info("启动成功");
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
--- app/web/src/main/java/com/yyh/base/biz/restapi/HelloController.java
+++ app/web/src/main/java/com/yyh/base/biz/restapi/HelloController.java
@@ -1,13 +1,16 @@
 package com.yyh.base.biz.restapi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/api/test")
 public class HelloController {
+	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
 	@GetMapping("/hello")
 	public void hello() {
-		System.out.println(1);
+		logger.info("请求成功了");
 	}
 }
