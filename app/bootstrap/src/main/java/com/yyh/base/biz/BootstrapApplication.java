7a48054ba24d339a47ce71e1e1288685be3f7517
yeyinghao <1113531030@qq.com>
2022-05-26 09:38:59 +0800

    创建模块

--- app/bootstrap/src/main/java/com/yyh/base/biz/BootstrapApplication.java
+++ app/bootstrap/src/main/java/com/yyh/base/biz/BootstrapApplication.java
@@ -3,6 +3,7 @@ package com.yyh.base.biz;
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 程序入口-启动类
@@ -12,8 +13,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date: 2022/5/24 21:54
  * @description: BootstrapApplication.java
  */
-@SpringBootApplication(scanBasePackages = "com.yyh.base")
+@ComponentScan(basePackages = "com.yyh.base")
 @NacosPropertySource(dataId = "base.yml", autoRefreshed = true)
+@SpringBootApplication
 public class BootstrapApplication {
 
 	public static void main(String[] args) {
